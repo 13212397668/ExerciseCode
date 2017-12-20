c794427d8de027254ad481cfc254ab8f325186a8
lx <82093444@qq.com>
2017-12-20 09:41:31 +0800

    练习代码

---
+++ VueCli/vueclitest/src/components/Hi.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+        <h2>{{ msg }}</h2>
+        <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hi",
+  data() {
+    return {
+      msg: "我是Hi页面"
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ VueCli/vueclitest/src/components/Hi2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }} -- 用户名：{{ $route.params.userName }} -- 用户ID：{{ $route.params.id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hi2",
+  data() {
+    return {
+      msg: "我是Hi页面-2"
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
